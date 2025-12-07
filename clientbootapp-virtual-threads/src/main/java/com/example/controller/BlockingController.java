18b8c9a53c2a992664286e0601147c3cc7cb218f
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-12-08 01:52:44 +0530

    remote-server

---
+++ clientbootapp-virtual-threads/src/main/java/com/example/controller/BlockingController.java
@@ -0,0 +1,31 @@
+package com.example.controller;
+
+import com.example.client.RemoteServiceClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlockingController {
+    private static final Logger log = LoggerFactory.getLogger(BlockingController.class);
+
+    private final RemoteServiceClient remoteServiceClient;
+
+    public BlockingController(RemoteServiceClient remoteServiceClient) {
+        this.remoteServiceClient = remoteServiceClient;
+    }
+
+    @GetMapping("/blocking/{seconds}")
+    public ResponseEntity<String> block(@PathVariable("seconds") Integer seconds) throws InterruptedException {
+        log.info("Received the request with seconds : {} ", seconds);
+        return ResponseEntity.ok(remoteServiceClient.invokeBlockingService(seconds));
+    }
+
+    @GetMapping("/currentThread")
+    public String currentThread() {
+        return Thread.currentThread().toString();
+    }
+}
